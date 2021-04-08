42e120ea8eca9cc98e3751b5a2ea2cfa6b724622
Cameron <cameronhunt88@hotmail.com>
2021-04-08 17:11:46 +0100

    Added while true loop so can find multiple colours

--- src/main/java/com/cameron/colourcodefinder/Search.java
+++ src/main/java/com/cameron/colourcodefinder/Search.java
@@ -10,29 +10,34 @@ public class Search extends ImportColours{
         // Getting the arrayLists from ImportColours to be used within Search class
         ArrayList<String> colourNames = getColourNames();
         ArrayList<String> colourNamesComplete = getColourNamesComplete();
-
-        System.out.println("Enter the colour to search for (enter help for information): ");
-        Scanner scanner = new Scanner(System.in);
-        String itemToFind = scanner.nextLine();
-        if(itemToFind.equalsIgnoreCase("help")){
-            System.out.println("--- HELP");
-            System.out.println("--- Please note: If two hex codes are returned when you search for one colour, it is because two of the same colour exist and they are slightly different.");
-            System.out.println("--- List of available colours:");
-            // print out all colour's and their hex code
-            for (String colourName : colourNames) {
-                System.out.println(colourName);
+        while(true) {
+            System.out.println("Enter the colour to search for (enter help for information or quit to exit): ");
+            Scanner scanner = new Scanner(System.in);
+            String itemToFind = scanner.nextLine();
+            if(itemToFind.equalsIgnoreCase("help")) {
+                System.out.println("--- HELP");
+                System.out.println("--- Please note: If two hex codes are returned when you search for one colour, it is because two of the same colour exist and they are slightly different.");
+                System.out.println("--- To exit the program, enter 'quit'.");
+                System.out.println("--- List of available colours:");
+                // print out all colour's and their hex code
+                for (String colourName : colourNames) {
+                    System.out.println(colourName);
+                }
+                System.out.println("Enter the colour to search for: ");
+                itemToFind = scanner.nextLine();
             }
-            System.out.println("Enter the colour to search for: ");
-            itemToFind = scanner.nextLine();
-        }
-
-        // Search colourNamesComplete arrayList and if colour is found, print output
-        int index = 0;
-        for(String string : colourNamesComplete){
-            if(string.matches(itemToFind)){
-                System.out.println(colourNamesComplete.get(index+1));
+            if (itemToFind.contains("quit")) {
+                System.exit(0);
             }
-            index++;
+
+            // Search colourNamesComplete arrayList and if colour is found, print output
+            int index = 0;
+            for(String string : colourNamesComplete){
+                if(string.matches(itemToFind)){
+                    System.out.println(colourNamesComplete.get(index+1));
+                }
+                index++;
+            }
         }
     }
 }
